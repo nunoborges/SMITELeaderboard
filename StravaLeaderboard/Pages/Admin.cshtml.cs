c7f52faf065e1c8088f01bedc2be16bd846e2d00
nuno_borges@hotmail.com <M0ththub>
2018-02-04 23:03:54 -0500

    successfully posted data to db on admin page

--- StravaLeaderboard/Pages/Admin.cshtml.cs
+++ StravaLeaderboard/Pages/Admin.cshtml.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StravaLeaderboard.Data;
+using StravaLeaderboard.models;
 
 namespace StravaLeaderboard.Pages
 {
-    public class ContactModel : PageModel
+    public class ClubModel : PageModel
     {
+        
+        private readonly DataContext _db;
+
+        public ClubModel(DataContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
         public string Message { get; set; }
+
+        [BindProperty]
+        public Club club { get; set; }
 
         public void OnGet()
         {
             Message = "Your contact page.";
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Clubs.Add(club);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("/Index");
         }
     }
 }
