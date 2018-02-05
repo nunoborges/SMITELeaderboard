f8efa9b4b129857a17bde0d8eb4405c0f25dcfd9
nuno_borges@hotmail.com <M0ththub>
2018-02-05 09:04:14 -0500

    created a club page to play with jqwidgets

---
+++ StravaLeaderboard/Pages/Club.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StravaLeaderboard.Data;
+using StravaLeaderboard.models;
+
+namespace StravaLeaderboard.Pages
+{
+    public class ClubTableModel : PageModel
+    {
+        private readonly StravaLeaderboard.Data.DataContext _context;
+
+        public ClubTableModel(StravaLeaderboard.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Club> Club { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Club = await _context.Clubs.ToListAsync();
+        }
+    }
+}
